f2b603f359958adb84e68ebe0c3b0757746e9692
Ahmad Abbas <75692454+ahmadabbas02@users.noreply.github.com>
2024-04-01 18:24:02 +0300

    fix(documents): studentId response null for medical report

--- src/main/java/com/ahmadabbas/filetracking/backend/document/medical/payload/MedicalReportDocumentMapper.java
+++ src/main/java/com/ahmadabbas/filetracking/backend/document/medical/payload/MedicalReportDocumentMapper.java
@@ -16,6 +16,7 @@ public interface MedicalReportDocumentMapper {
     @Mapping(source = "category.name", target = "categoryName")
     @Mapping(source = "category.categoryId", target = "categoryId")
     @Mapping(source = "category.parentCategoryId", target = "categoryParentId")
+    @Mapping(source = "student.id", target = "studentId")
     @Mapping(source = "student.user.firstName", target = "studentFirstName")
     @Mapping(source = "student.user.lastName", target = "studentLastName")
     @Mapping(source = "student.user.fullName", target = "studentFullName")
